55ee149240817d9c48c3982180cac202d12d8618
Jan Stümmel <info@janstuemmel.de>
2017-05-09 14:39:37 +0200

    fix(controller): fix create among the FHIR spec
    * bump fhir-proof version
    * added tests
    * added util operationOutcome

--- test/spec/controller/createSpec.js
+++ test/spec/controller/createSpec.js
@@ -5,16 +5,22 @@ const fhir = require('../../../');
 
 describe('createSpec', () => {
 
-  var app, req;
+  var app, req, sendPost;
 
   beforeEach(() => {
     app = express();
     app.use(fhir({}));
     req = supertest(app);
+    sendPost = (route, resource, expectedStstaus) => {
+      return req.post(route)
+        .set('Content-Type', 'application/json')
+        .send(JSON.stringify(resource))
+        .expect(expectedStstaus);
+    };
   });
 
 
-  it('should create resource', async () => {
+  it('should create', async () => {
 
     // given
     var resource = {
@@ -24,56 +30,137 @@ describe('createSpec', () => {
     };
 
     // when
-    await req.post('/Observation')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify(resource))
-      .expect(200)
-      .then(res => {
+    await sendPost('/Observation', resource, 200).then(res => {
 
-        // then
-        expect(JSON.parse(res.text)).toMatchObject({
-          err: false,
-          data: expect.objectContaining(assign({}, resource, {
-            id: expect.any(String)
-          }))
-        });
-      });
+      // then
+      expect(JSON.parse(res.text)).toMatchObject(assign({}, resource, {
+        id: expect.any(String)
+      }));
+    });
 
   });
 
 
-  it('should not create resource on wrong statusCode', async () => {
+  it('should create with new id and new meta', async () => {
 
     // given
     var resource = {
+      id: 'oldid',
+      meta: {
+        versionId: 'oldverionid',
+        lastupdated: '2000-01-01'
+      },
       resourceType: 'Observation',
-      status: 'wrongStatus!', // FAIL
+      status: 'final',
       code: { text: 'test' },
     };
 
     // when
-    await req.post('/Observation')
-      .set('Content-Type', 'application/json')
-      .send(JSON.stringify(resource))
-      .expect(400)
-      .then(res => {
+    await sendPost('/Observation', resource, 200).then(res => {
 
-        // then
-        expect(JSON.parse(res.text)).toMatchObject({
-          err: true,
-          msg: expect.any(String)
-        });
-      });
+      // then
+      expect(JSON.parse(res.text).id).not.toBe('oldid');
+      expect(JSON.parse(res.text).meta.versionId).not.toBe('oldversionId');
+      expect(JSON.parse(res.text).meta.lastUpdated).not.toBe('2000-01-01');
+    });
+
   });
 
 
-  it('should not create resource on wrong resourceType', async () => {
+  it('should create with HEADER.Location', async () => {
+
+    // given
+    var resource = {
+      resourceType: 'Observation',
+      status: 'final',
+      code: { text: 'test' },
+    };
+
+    // when
+    await sendPost('/Observation', resource, 200).then(res => {
+
+      // then
+      expect(res.header).toHaveProperty('location');
+      expect(res.header.location).toMatch(/^http.+\/Observation\/.+\/_history\/.+$/);
+    });
+
+  });
+
+
+  it('should create resource with id and meta', async () => {
+
+    // given
+    var resource = {
+      resourceType: 'Observation',
+      status: 'final',
+      code: { text: 'test' },
+    };
+
+    // when
+    await sendPost('/Observation', resource, 200).then(res => {
+
+      // then
+      expect(JSON.parse(res.text)).toHaveProperty('id');
+      expect(JSON.parse(res.text)).toHaveProperty('meta.versionId');
+      expect(JSON.parse(res.text)).toHaveProperty('meta.lastUpdated');
+    });
+
+  });
+
+
+  it('should not create on wrong input', async () => {
+
+    // given
+    var resource = {
+      resourceType: 'Observation',
+      status: 'wrongStatusCode!!', // FAIL
+      code: { text: 'test' },
+    };
+
+    // when
+    await sendPost('/Observation', resource, 400).then(res => {
+
+      // then
+      expect(JSON.parse(res.text).issue[0]).toMatchObject({
+        severity: 'error',
+        code: 'BAD_REQUEST',
+        details: { text: 'Cannot create resource' }
+      });
+    });
+
+  });
+
+
+  it('should not create on wrong resourceType', async () => {
+
+    // given
+    var resource = {
+      resourceType: 'NotFHIRResource',
+      status: 'final',
+      code: { text: 'test' },
+    };
+
+    // when
+    await sendPost('/Observation', resource, 404).then(res => {
+
+      // then
+      expect(JSON.parse(res.text).issue[0]).toMatchObject({
+        severity: 'error',
+        code: 'NOT_FOUND',
+        details: { text: 'Wrong resourceType' }
+      });
+    });
+
+  });
+
+
+  it('should not create on wrong resource URI', async () => {
 
     // when
     const res = req.post('/NoFHIRResource');
 
     // then
-    await res.expect(400);
+    await res.expect(404);
   });
 
 });
